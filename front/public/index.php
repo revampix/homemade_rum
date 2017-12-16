44d33e352d8132f93fb99feaa1d21840528b7aa1
Tsvetan Stoychev <ceckoslab@gmail.com>
2017-12-16 16:53:53 +0100

    Adding quick hack for allowing Cross Origin Requests.

--- front/public/index.php
+++ front/public/index.php
@@ -1,6 +1,19 @@
 <?php
 ini_set('display_errors', 1);
 
+// Hacking quickly to handle Cross Origin Requests
+// Better if we implemente this in NIGIX level
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+    header('Access-Control-Allow-Headers: X-Requested-With, content-type, access-control-allow-origin, access-control-allow-methods, access-control-allow-headers');
+    header('Access-Control-Max-Age: 86400');
+
+    exit;
+}
+
+header('Access-Control-Allow-Origin: *');
+
 require_once  __DIR__ . '/../autoload.php';
 
 
